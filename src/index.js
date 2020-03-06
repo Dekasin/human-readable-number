2eb859ee99c599c97e398fdf028e90d687f7fe8e
dekasins <dekasins@gmail.com>
2020-03-06 11:49:08 +0300

    fix: grammar failure

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ module.exports = function toReadable (number) {
   let res ='';
   let strNumb = number + "";
   let arrNumb=strNumb.split('')
-  //let arr = {"1": "one", "2":"two", "3":"thre", "4":"four", "5":"five", '6': "secs", "7": 'seven', "8": "eght", "9":"nine", "10": "ten", "11": "eleven"};
+  //let arr = {"1": "one", "2":"two", "3":"three", "4":"four", "5":"five", '6': "secs", "7": 'seven', "8": "eight", "9":"nine", "10": "ten", "11": "eleven"};
 
-  let arr20 = ["zero","one", "two", "three", "four", "five", "six", 'seven', "eight", "nine", "ten", "eleven", "twelf", "thirteen", "fourteen","fifteen", "sixteen", "seventeen","eighteen","nineteen"];
+  let arr20 = ["zero","one", "two", "three", "four", "five", "six", 'seven', "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen", "sixteen", "seventeen","eighteen","nineteen"];
 
-  let arr100 = ["zero","ten", "twenty", "thirty", "fourty", "fifty", "sixty", 'seventy', "eighty", "ninty"];
+  let arr100 = ["zero","ten", "twenty", "thirty", "forty", "fifty", "sixty", 'seventy', "eighty", "ninety"];
 
     if( number < 20) {
      res = arr20[number];
@@ -16,7 +16,7 @@ module.exports = function toReadable (number) {
      }
 
      if (100 <=number && number <1000) {
-      res =arr20[arrNumb[0]] + " hundreed " + arr100[arrNumb[1]] + " "+arr20[arrNumb[2]];
+      res =arr20[arrNumb[0]] + " hundred " + arr100[arrNumb[1]] + " "+arr20[arrNumb[2]];
      }
   // for (let i = number.length-1; i > 0; i--) {
   //     res.unshift[i];
