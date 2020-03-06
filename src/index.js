8690a5b701f0cbc98118fac588e84d8c89e8b637
dekasins <dekasins@gmail.com>
2020-03-06 12:27:17 +0300

    fix: fix number with type *XX, when XX < 20

--- src/index.js
+++ src/index.js
@@ -4,15 +4,26 @@ module.exports = function toReadable (number) {
   let arrNumb=strNumb.split('')
   //let arr = {"1": "one", "2":"two", "3":"three", "4":"four", "5":"five", '6': "secs", "7": 'seven', "8": "eight", "9":"nine", "10": "ten", "11": "eleven"};
 
-  let arr20 = ["zero","one", "two", "three", "four", "five", "six", 'seven', "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen", "sixteen", "seventeen","eighteen","nineteen"];
+  let arr20 = ["","one", "two", "three", "four", "five", "six", 'seven', "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen", "sixteen", "seventeen","eighteen","nineteen"];
 
-  let arr100 = ["zero","ten", "twenty", "thirty", "forty", "fifty", "sixty", 'seventy', "eighty", "ninety"];
+  let arr100 = ["","ten", "twenty", "thirty", "forty", "fifty", "sixty", 'seventy', "eighty", "ninety"];
 
-    if( number < 20) {
-     res = arr20[number];
-     }
+    if( number === 0) {
+      return 'zero';
+      };
+
+    if( number < 20 && number != 0) {
+      return arr20[number];
+      }
+
      if (20 <=number && number <100) {
-      res =arr100[arrNumb[0]] + " " + arr20[arrNumb[1]];
+      res = arr100[arrNumb[0]] + " " + arr20[arrNumb[1]];
+      return res.trim()
+     }
+
+     if (100 <=number && number <1000 && arrNumb[1] < 2) {
+      res = arr20[arrNumb[0]] + " hundred " + arr20[1*(arrNumb[1] + arrNumb[2])];
+      return res.trim();
      }
 
      if (100 <=number && number <1000) {
@@ -21,5 +32,5 @@ module.exports = function toReadable (number) {
   // for (let i = number.length-1; i > 0; i--) {
   //     res.unshift[i];
   // }
- return res;   
+ return res.trim();   
 }  
